1ecd045f8b36394ea8526cc4088b87c4e75c3c88
EiseleL <luca.eisele@hak-reutte.ac.at>
2024-02-20 13:19:32 +0100

    New variable bonushealth

--- Project_BattleArea_Eisele_Wolf/src/BattleArena/FighterCharacter.java
+++ Project_BattleArea_Eisele_Wolf/src/BattleArena/FighterCharacter.java
@@ -4,6 +4,7 @@ public abstract class FighterCharacter {
 
 	private String name;
 	private int health;
+	private int bonushealth;
 	private boolean specialAbilityActive;
 
 	public FighterCharacter(String name) {
@@ -20,6 +21,11 @@ public abstract class FighterCharacter {
 	public int getHealth() {
 		return health;
 	}
+	
+
+	public int getBonushealth() {
+		return bonushealth;
+	}
 
 	public boolean isSpecialAbilityActive() {
 		return specialAbilityActive;
@@ -32,27 +38,39 @@ public abstract class FighterCharacter {
 	public void setHealth(int health) {
 		this.health = health;
 	}
+	
+
+	public void setBonushealth(int bonushealth) {
+		this.bonushealth = bonushealth;
+	}
 
 	public void setSpecialAbilityActive(boolean specialAbilityActive) {
 		this.specialAbilityActive = specialAbilityActive;
 	}
 	
 	public void gotDamage(int points) {
-		if(!((this.health - points) < 0)) {
-			this.health -= points;
+		if(this.bonushealth < 0) {
+			if(!((this.bonushealth - points) >= 0)) {
+				points -= this.bonushealth;
+				this.bonushealth = 0;
+				
+				if(!((this.health - points) < 0)) {
+					this.health -= points;
+				}else{
+					this.health = 0;
+				}
+			}
 		}
+		
+		
 	}
 	
-	public void activateAbility() {
-		this.setSpecialAbilityActive(true);
-	}
+	public abstract void activateAbility();
 	
-	public void deactivateAbility() {
-		this.setSpecialAbilityActive(false);
-	}
+	public abstract void deactivateAbility();
 	
+	public abstract int attack();
 	
-	public abstract void attack();
 	
 	
 	
