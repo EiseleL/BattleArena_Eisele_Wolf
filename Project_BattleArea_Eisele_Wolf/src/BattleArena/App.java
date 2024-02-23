3ae7590eb8711d4d03d9bd3b77ee142b1a0a25ee
EiseleL <luca.eisele@hak-reutte.ac.at>
2024-02-23 15:22:56 +0100

    Create App Class

--- Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
+++ Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
@@ -2,4 +2,13 @@ package BattleArena;
 
 public class App {
 
+	public static void main(String[] args) {
+		
+		Dragon d1 = new Dragon("Honig Zahn","200", boolean specialAbilityActive) {
+		
+		Arena Arena = new Arena(, null);
+		
+	}
+	
+
 }
