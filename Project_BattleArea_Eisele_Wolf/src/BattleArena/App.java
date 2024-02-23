df6f4ab949c54ad9cb98e2498a463398c9f741b0
EiseleL <luca.eisele@hak-reutte.ac.at>
2024-02-23 15:23:36 +0100

    Create two fighterCharacters in App Class

--- Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
+++ Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
@@ -4,9 +4,11 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Dragon d1 = new Dragon("Honig Zahn","200", boolean specialAbilityActive) {
+		Dragon d1 = new Dragon("Honig Zahn");
+		Dwarf z1 = new Dwarf("Erwin");
 		
-		Arena Arena = new Arena(, null);
+		Arena Arena = new Arena(d1, z1);
+		
 		
 	}
 	
