01034572f9729437592a469c95e4e305e81b62d1
EiseleL <luca.eisele@hak-reutte.ac.at>
2024-03-05 12:24:10 +0100

    Create Text output

--- Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
+++ Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
@@ -4,6 +4,15 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		
+		System.out.println("Spieler 1: Wähle deinen Spielcharakter");
+		System.out.println();
+		System.out.println("1. Dragon");
+		System.out.println("2. Dwarf");
+		System.out.println();
+		
+		
+		
 		Dragon d1 = new Dragon("Honig Zahn");
 		Dwarf z1 = new Dwarf("Erwin");
 		
