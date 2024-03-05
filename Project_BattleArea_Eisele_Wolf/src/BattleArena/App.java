2ae8752ee7547dc1f05e229ec0596837dfb70ca3
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-03-05 12:21:52 +0100

    Changed Scanner

--- Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
+++ Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
@@ -1,16 +1,19 @@
 package BattleArena;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
 		Dragon d1 = new Dragon("Honig Zahn");
 		Dwarf z1 = new Dwarf("Erwin");
 		
 		Arena Arena = new Arena(d1, z1);
 		
-		Arena.fight();
+		Arena.fight(scanner);
 		
-		
+		scanner.close();
 	}
 }
