7f202ce8fa79fcf3b30893e7a7fc4bf3ca676f54
EiseleL <luca.eisele@hak-reutte.ac.at>
2024-03-05 12:46:22 +0100

    Complete Class App

--- Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
+++ Project_BattleArea_Eisele_Wolf/src/BattleArena/App.java
@@ -6,23 +6,63 @@ public class App {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int wahl = 0;
+		String name = "";
 		
+		FighterCharacter p1 = null;
+		FighterCharacter p2 = null;
 		
-		System.out.println("Spieler 1: Wähle deinen Spielcharakter");
-		System.out.println();
-		System.out.println("1. Dragon");
-		System.out.println("2. Dwarf");
-		System.out.println();
+		for(int i = 1; i < 3; i++) {
+			System.out.println("Spieler " + i +": Wähle deinen Spielcharakter");
+			System.out.println();
+			System.out.println("1. Dragon");
+			System.out.println("2. Dwarf");
+			System.out.println();
+			boolean fw = true;
+			
+			do {
+				 wahl = scanner.nextInt();
+				if(wahl == 1|| wahl == 2  ){
+					fw =false;
+				}} while(fw);
+			
+			System.out.println("Spieler " + i +": Gib deinen Spielnamen an:");
+			
+			
+			do {
+				 name = scanner.next();
+				
+				} while(name.equals(""));
+			
+				if(p1 != null) {
+					wahl += 10;
+				}
+				switch (wahl) {
+				case 1: 
+					p1 = new Dragon(name);
+					break;
+					
+				case 2:
+					 p1 = new Dwarf(name);
+					break;
+					
+				case 11: 
+					p2 = new Dragon(name);
+					break;
+					
+				case 12:
+					p2 = new Dwarf(name);
+					break;
+				}
+		}
 		
-		
-		
-		Dragon d1 = new Dragon("Honig Zahn");
-		Dwarf z1 = new Dwarf("Erwin");
-		
-		Arena Arena = new Arena(d1, z1);
+		Arena Arena = new Arena(p1, p2);
 		
 		Arena.fight(scanner);
 		
 		scanner.close();
+		
+		
+		
 	}
 }
